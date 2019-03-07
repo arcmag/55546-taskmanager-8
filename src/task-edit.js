f7ea5dba72873d6b5762699c39869aaed082f19f
Николай <arcmag-work@mail.ru>
2019-03-07 20:25:52 +0700

    Убирает лишний bind

--- src/task-edit.js
+++ src/task-edit.js
@@ -176,7 +176,7 @@ class TaskEdit {
 
   bind() {
     this._eventSubmit = this._onSubmitButtonClick.bind(this);
-    this._form.addEventListener(`submit`, this._eventSubmit.bind(this));
+    this._form.addEventListener(`submit`, this._eventSubmit);
   }
 
   unbind() {
